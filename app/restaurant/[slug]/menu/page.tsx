8f66193b834249a846f526fdbff58f9d2cf61b2e
punkzberryz <16286841+punkzberryz@users.noreply.github.com>
2023-05-17 17:11:14 +0800

    first commit

---
+++ app/restaurant/[slug]/menu/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import RestaurantNavBar from "../components/RestaurantNavBar";
+import Menu from "../components/Menu";
+export default function RestaurantMenu() {
+  return (
+    <div className="bg-white w-[100%] rounded p-3 shadow">
+      <RestaurantNavBar />
+      <Menu />
+    </div>
+  );
+}
