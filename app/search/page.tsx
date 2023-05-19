1979555d3166488307e3034670eddd170eb3c44e
punkzberryz <16286841+punkzberryz@users.noreply.github.com>
2023-05-19 20:35:22 +0800

    update search queries function

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,9 +2,16 @@ import type { Metadata } from "next";
 import Header from "./components/Header";
 import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
-import { PrismaClient } from "@prisma/client";
+import { PRICE, PrismaClient } from "@prisma/client";
+import { Suspense } from "react";
 
 const prisma = new PrismaClient();
+
+interface SearchParams {
+  city: string;
+  cuisine?: string;
+  price?: PRICE;
+}
 
 const select = {
   id: true,
@@ -16,17 +23,48 @@ const select = {
   slug: true,
 };
 
-const fetchRestaurantsByCity = (city: string | undefined) => {
-  if (!city) return prisma.restaurant.findMany({ select });
+const fetchLocations = async () => {
+  const locations = await prisma.location.findMany();
+  if (!locations) throw new Error();
+  return locations;
+};
+
+const fetchCuisines = async () => {
+  const cuisines = await prisma.cuisine.findMany();
+
+  if (!cuisines) throw new Error();
+  return cuisines;
+};
+
+const fetchRestaurantsByCity = (searchParams: SearchParams) => {
+  const where: any = {};
+  if (searchParams.city) {
+    const location = {
+      name: {
+        equals: searchParams.city.toLowerCase(),
+      },
+    };
+    where.location = location;
+  }
+  if (!searchParams.city) return prisma.restaurant.findMany({ select });
+
+  if (searchParams.cuisine) {
+    const cuisine = {
+      name: {
+        equals: searchParams.cuisine.toLocaleLowerCase(),
+      },
+    };
+    where.cuisine = cuisine;
+  }
+  if (searchParams.price) {
+    const price = {
+      equals: searchParams.price,
+    };
+    where.price = price;
+  }
 
   return prisma.restaurant.findMany({
-    where: {
-      location: {
-        name: {
-          equals: city,
-        },
-      },
-    },
+    where,
     select,
   });
 };
@@ -39,16 +77,21 @@ export const metadata: Metadata = {
 export default async function Search({
   searchParams,
 }: {
-  searchParams: { city: string };
+  searchParams: SearchParams;
 }) {
-  const city = searchParams.city.toLocaleLowerCase();
-  const restaurants = await fetchRestaurantsByCity(city);
-
+  const restaurants = await fetchRestaurantsByCity(searchParams);
+  const locations = await fetchLocations();
+  const cuisines = await fetchCuisines();
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSideBar />
+        <SearchSideBar
+          locations={locations}
+          cuisines={cuisines}
+          searchParams={searchParams}
+        />
+
         <div className="w-5/6">
           {restaurants.length ? (
             restaurants.map((restaurant) => (
